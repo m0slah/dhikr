f5928b170633a078df8d281443e3d363e88be1d3
m0slah <muhammedslahqadir@gmail.com>
2023-03-16 17:27:08 +0300

    fetch data from database

--- components/dhikr/DhikrList.js
+++ components/dhikr/DhikrList.js
@@ -1,40 +1,44 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect, useCallback } from "react";
 
 import Card from "../UI/Card";
 import Cart from "./Cart/Cart";
 import classes from "./DhikrList.module.css";
 
-const DHIKR = [
-  {
-    id: 1,
-    name: "سبحان اللە",
-  },
-  {
-    id: 2,
-    name: "الحمد اللە",
-  },
-  {
-    id: 3,
-    name: "اللە اکبر",
-  },
-  {
-    id: 4,
-    name: "استغفراللە",
-  },
-  {
-    id: 5,
-    name: "أشهد أن لا إله إلا الله وأشهد أن محمدا رسول الله",
-  },
-  {
-    id: 6,
-    name: "لا اله الا الله",
-  },
-];
-
 const DhikrList = () => {
+  const [dhikrData, setDhikrData] = useState([]);
   const [counters, setCounters] = useState(0);
 
-  const countHandler = (item) => {
+  const fetchDhikrHandler = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://dhikr-1ccd5-default-rtdb.firebaseio.com/dhikr.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Some thing Went Wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedData = [];
+
+      for (const key in responseData) {
+        loadedData.push({
+          id: key,
+          title: responseData[key].title,
+        });
+      }
+      setDhikrData(loadedData);
+    } catch (error) {
+      alert(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchDhikrHandler();
+  }, [fetchDhikrHandler]);
+
+  const countHandler = () => {
     const enteredAmount = counters + 1;
     setCounters(enteredAmount);
   };
@@ -43,11 +47,11 @@ const DhikrList = () => {
     <dev className={classes.dhikr}>
       <Cart onCounters={counters} />
       <Card>
-        {DHIKR.map((item) => (
+        {dhikrData.map((item) => (
           <ul className={classes.ul} id={item.id} key={item.id}>
             <li>
               <div>
-                <h3>{item.name}</h3>
+                <h3>{item.title}</h3>
                 <button className={classes.btn} onClick={countHandler}>
                   ژماردن
                 </button>
